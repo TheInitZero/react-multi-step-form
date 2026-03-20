8fcd772c4d37f5b31f01cc64124c3a83750dcfbc
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-03-20 17:09:00 +0530

    chore: add Ubuntu font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './app.tsx';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/700.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
