af7f362df6bd2521dae1c338c339cf909f364d20
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-03-21 11:45:51 +0530

    refactor: enhance accessibility in ProgressStep component
    Provide accessible label and description to `li`.

--- src/features/signup-progress/progress-step.tsx
+++ src/features/signup-progress/progress-step.tsx
@@ -1,21 +1,35 @@
+import { useId } from 'react';
 import clsx from 'clsx';
 
 type Props = {
   title: string;
-  status: { kind: 'NotStarted' | 'Current' | 'Completed'; description: string };
+  status: {
+    kind: 'NotStarted' | 'Current' | 'Completed';
+    description: string;
+  };
 };
 
 export default function ProgressStep({ title, status }: Props) {
+  const titleId = useId();
+  const descId = useId();
+
   return (
     <li
-      aria-current={status.kind == 'Current' ? 'step' : undefined}
-      data-progress-step={status.kind}
+      aria-current={status.kind === 'Current' ? 'step' : undefined}
+      aria-labelledby={titleId}
+      aria-describedby={descId}
       className="flex items-center gap-2"
     >
-      <p className={clsx('sr-only sm:not-sr-only', status.kind != 'Current' && 'sm:text-black/45')}>
+      <p
+        id={titleId}
+        className={clsx('sr-only sm:not-sr-only', status.kind != 'Current' && 'sm:text-black/45')}
+      >
         {title}
       </p>
-      <span className="sr-only">{status.description}</span>
+
+      <span id={descId} className="sr-only">
+        {status.description}
+      </span>
     </li>
   );
 }
