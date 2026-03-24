3ca78012e565253b5914b84b053341a724ea999c
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-03-24 23:17:46 +0530

    feat: add Summary component with tests and integrate it into the main application

---
+++ src/features/signup-form/summary/summary.tsx
@@ -0,0 +1,75 @@
+import { calculateTotalPrice, getPriceLabel, type BillingPeriod } from '../utils';
+
+type Props = {
+  subscription: { name: string; billingPeriod: BillingPeriod; price: number };
+  addOns: { name: string; price: number }[];
+  onSubscriptionChange: () => void;
+};
+
+export default function Summary({ subscription, addOns, onSubscriptionChange }: Props) {
+  const addOnsList = addOns.map((addOn) => (
+    <li className="flex items-center justify-between">
+      <span className="text-black/45">{addOn.name}</span>
+      <span>
+        +{getPriceLabel({ amount: addOn.price, billingPeriod: subscription.billingPeriod })}
+      </span>
+    </li>
+  ));
+
+  const totalPrice = calculateTotalPrice(
+    subscription.price,
+    addOns.map(({ price }) => price),
+  );
+
+  return (
+    <section className="space-y-4">
+      <div className="space-y-2">
+        <h2 className="text-xl">Finishing up</h2>
+        <p className="text-black/45">Double-check everything looks OK before confirming.</p>
+      </div>
+
+      <div className="space-y-4 rounded-2xl border-2 border-gray-200 p-4 shadow-sm">
+        <section aria-label="Subscription" className="space-y-2">
+          <p className="flex items-center justify-between">
+            <span>
+              {subscription.name} ({subscription.billingPeriod})
+            </span>{' '}
+            <span>
+              {getPriceLabel({
+                amount: subscription.price,
+                billingPeriod: subscription.billingPeriod,
+              })}
+            </span>
+          </p>
+          <button
+            type="button"
+            className="text-blue-700 underline underline-offset-2 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500"
+            onClick={onSubscriptionChange}
+          >
+            Change subscription
+          </button>
+        </section>
+
+        {addOnsList.length > 0 && <ul aria-label="Add-ons">{addOnsList}</ul>}
+
+        <hr className="border-t-2 border-gray-200" />
+
+        <p className="flex items-center justify-between">
+          <span>Total ({subscription.billingPeriod})</span>{' '}
+          <span>
+            {getPriceLabel({ amount: totalPrice, billingPeriod: subscription.billingPeriod })}
+          </span>
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <button type="button" className="btn-ghost">
+          Go Back
+        </button>
+        <button type="submit" className="btn-primary">
+          Confirm
+        </button>
+      </div>
+    </section>
+  );
+}
