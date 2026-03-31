f6655cc897d678597cb8ed2c0dbd42b5dd5ec70d
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-03-31 18:25:55 +0530

    feat: integrate input state machine into app

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import SignupForm from './features/signup-form/signup-form';
 import YourInfo from './features/signup-form/your-info/your-info';
 import SelectPlan from './features/signup-form/select-plan/select-plan';
 import AddOns from './features/signup-form/add-ons/add-ons';
-import Summary from './features/signup-form/summary/summary';
+import Summary, { type SummaryProps } from './features/signup-form/summary/summary';
 import ConfirmationMessage from './features/confirmation/confirmation-message';
 import InfoInput, { type InfoInputProps } from './features/signup-form/your-info/info-input';
 import { none } from './utils';
@@ -16,141 +16,210 @@ import type { AddOnsOptionProps } from './features/signup-form/add-ons/add-ons-o
 import AddOnsOption from './features/signup-form/add-ons/add-ons-option';
 import { formSteps, formStepStatusDescriptions } from './features/state/form-progress-state';
 import { useFormProgressMachine } from './features/hooks/use-form-progress-machine';
+import { useInputStateMachine } from './features/hooks/use-input-state-machine';
+import { addOns, subscriptions } from './features/state/input-state';
 
 export default function App() {
   const formProgressActor = useFormProgressMachine();
+  const inputActor = useInputStateMachine();
 
-  const progressStepConfigs: ProgressStepProps[] = Object.entries(
-    formProgressActor.snapshot.context.statusRecord,
-  ).map(function ([stepId, stepStatus]) {
+  const progressStepConfigs: ProgressStepProps[] = (function () {
+    const { statusRecord } = formProgressActor.snapshot.context;
+    const statusEntries = Object.entries(statusRecord);
+
+    return statusEntries.map(function (entry) {
+      const [stepId, stepStatus] = entry;
+      const title = formSteps[stepId].title;
+      const description = formStepStatusDescriptions[stepStatus];
+      const status = { kind: stepStatus, description };
+
+      return { title, status };
+    });
+  })();
+
+  const infoInputConfigs: InfoInputProps[] = (function () {
+    const { name, email, telephone } = inputActor.snapshot.context;
+
+    const nameInputConfig: InfoInputProps = (function () {
+      const inputType: InfoInputProps['inputType'] = { kind: 'text', autoComplete: 'name' };
+      const validationStatus = none();
+
+      const onInput: InfoInputProps['onInput'] = function (value) {
+        inputActor.send({ type: 'SET_NAME', name: value });
+      };
+
+      return {
+        value: name,
+        labelText: 'Name',
+        name: 'name',
+        placeholder: 'Stephen King',
+        required: true,
+        inputType,
+        validationStatus,
+        onInput,
+      };
+    })();
+
+    const emailInputConfig: InfoInputProps = (function () {
+      const inputType: InfoInputProps['inputType'] = { kind: 'email', autoComplete: 'email' };
+      const validationStatus = none();
+
+      const onInput: InfoInputProps['onInput'] = function (value) {
+        inputActor.send({ type: 'SET_EMAIL', email: value });
+      };
+
+      return {
+        value: email,
+        labelText: 'Email Address',
+        name: 'email',
+        placeholder: 'stephenking@lorem.com',
+        required: true,
+        inputType,
+        validationStatus,
+        onInput,
+      };
+    })();
+
+    const telephoneInputConfig: InfoInputProps = (function () {
+      const inputType: InfoInputProps['inputType'] = { kind: 'tel', autoComplete: 'tel' };
+      const validationStatus = none();
+
+      const onInput: InfoInputProps['onInput'] = function (value) {
+        inputActor.send({ type: 'SET_TELEPHONE', telephone: value });
+      };
+
+      return {
+        value: telephone,
+        labelText: 'Phone Number',
+        name: 'phone',
+        placeholder: '+1 234 567 890',
+        required: true,
+        inputType,
+        validationStatus,
+        onInput,
+      };
+    })();
+
+    return [nameInputConfig, emailInputConfig, telephoneInputConfig];
+  })();
+
+  const billingOptionConfigs: BillingOptionProps[] = (function () {
+    const { billingPeriod } = inputActor.snapshot.context;
+
+    const monthlyOptionConfig = (function () {
+      const checked = billingPeriod === 'Monthly';
+
+      function onChange() {
+        inputActor.send({ type: 'SET_BILLING_PERIOD', billingPeriod: 'Monthly' });
+      }
+
+      return {
+        value: 'monthly',
+        labelText: 'Monthly',
+        required: true,
+        checked,
+        onChange,
+      };
+    })();
+
+    const yearlyOptionConfig = (function () {
+      const checked = billingPeriod === 'Yearly';
+
+      function onChange() {
+        inputActor.send({ type: 'SET_BILLING_PERIOD', billingPeriod: 'Yearly' });
+      }
+
+      return {
+        value: 'yearly',
+        labelText: 'Yearly',
+        required: true,
+        checked,
+        onChange,
+      };
+    })();
+
+    return [monthlyOptionConfig, yearlyOptionConfig];
+  })();
+
+  const subscriptionOptionConfigs: SubscriptionOptionProps[] = Object.values(subscriptions).map(
+    function (subscription) {
+      const { billingPeriod, subscriptionId } = inputActor.snapshot.context;
+
+      const description: SubscriptionOptionProps['description'] = (function () {
+        const { price: rate, bonuses } = subscription;
+        const price = rate[billingPeriod];
+
+        return billingPeriod === 'Monthly'
+          ? { billingPeriod, price }
+          : { billingPeriod, price, bonuses };
+      })();
+
+      const config = (function () {
+        const { id, name } = subscription;
+        const checked = subscriptionId === id;
+
+        function onChange() {
+          inputActor.send({ type: 'SET_SUBSCRIPTION_ID', subscriptionId: id });
+        }
+
+        return {
+          value: id,
+          labelText: name,
+          required: true,
+          description,
+          checked,
+          onChange,
+        };
+      })();
+
+      return config;
+    },
+  );
+
+  const addOnsOptionConfigs: AddOnsOptionProps[] = Object.values(addOns).map(function (addOn) {
+    const { billingPeriod, addOnIds } = inputActor.snapshot.context;
+    const { id, name, description, price: rate } = addOn;
+    const checked = addOnIds.has(id);
+    const amount = rate[billingPeriod];
+    const price = { billingPeriod, amount };
+
+    function onChange() {
+      inputActor.send({ type: 'TOGGLE_ADD_ON', addOnId: id });
+    }
+
     return {
-      title: formSteps[stepId].title,
-      status: { kind: stepStatus, description: formStepStatusDescriptions[stepStatus] },
+      value: id,
+      labelText: name,
+      checked,
+      description,
+      price,
+      onChange,
     };
   });
 
-  const infoInputConfigs: InfoInputProps[] = [
-    {
-      labelText: 'Name',
-      name: 'name',
-      placeholder: 'Stephen King',
-      inputType: { kind: 'text', autoComplete: 'name' },
-      required: true,
-      value: '',
-      onInput: () => {},
-      validationStatus: none(),
-    },
+  const summaryProps: Omit<SummaryProps, 'footer'> = (function () {
+    const { billingPeriod, subscriptionId, addOnIds } = inputActor.snapshot.context;
 
-    {
-      labelText: 'Email Address',
-      name: 'email',
-      placeholder: 'stephenking@lorem.com',
-      inputType: { kind: 'email', autoComplete: 'email' },
-      required: true,
-      value: '',
-      onInput: () => {},
-      validationStatus: none(),
-    },
+    const subscription = (function () {
+      const selectedSubscription = subscriptions[subscriptionId];
+      const { name, price: rate } = selectedSubscription;
+      const price = rate[billingPeriod];
+      return { name, billingPeriod, price };
+    })();
 
-    {
-      labelText: 'Phone Number',
-      name: 'phone',
-      placeholder: '+1 234 567 890',
-      inputType: { kind: 'tel', autoComplete: 'tel' },
-      required: true,
-      value: '',
-      onInput: () => {},
-      validationStatus: none(),
-    },
-  ];
+    const addOnsProp = [...addOnIds].map(function (addOnId) {
+      const addOn = addOns[addOnId];
+      const { name, price: rate } = addOn;
+      const price = rate[billingPeriod];
+      return { name, price };
+    });
 
-  const billingOptionConfigs: BillingOptionProps[] = [
-    {
-      value: 'monthly',
-      labelText: 'Monthly',
-      required: true,
-      checked: true,
-      onChange: () => {},
-    },
+    function onSubscriptionChange() {
+      formProgressActor.send({ type: 'SUMMARY.CHANGE_SUBSCRIPTION' });
+    }
 
-    {
-      value: 'yearly',
-      labelText: 'Yearly',
-      required: true,
-      checked: false,
-      onChange: () => {},
-    },
-  ];
-
-  const subscriptionOptionConfigs: SubscriptionOptionProps[] = [
-    {
-      value: 'arcade',
-      required: true,
-      labelText: 'Arcade',
-      description: {
-        billingPeriod: 'Yearly',
-        price: 90,
-        bonuses: ['2 months free'],
-      },
-      onChange: () => {},
-      checked: true,
-    },
-
-    {
-      value: 'advanced',
-      required: true,
-      labelText: 'Advanced',
-      description: {
-        billingPeriod: 'Yearly',
-        price: 120,
-        bonuses: ['3 months free'],
-      },
-      onChange: () => {},
-      checked: false,
-    },
-
-    {
-      value: 'pro',
-      required: true,
-      labelText: 'Pro',
-      description: {
-        billingPeriod: 'Yearly',
-        price: 150,
-        bonuses: ['4 months free'],
-      },
-      onChange: () => {},
-      checked: false,
-    },
-  ];
-
-  const addOnsOptionConfigs: AddOnsOptionProps[] = [
-    {
-      value: 'online-service',
-      checked: false,
-      onChange: () => {},
-      labelText: 'Online service',
-      description: 'Access to multiplayer games',
-      price: { billingPeriod: 'Monthly', amount: 1 },
-    },
-
-    {
-      value: 'larger-storage',
-      checked: false,
-      onChange: () => {},
-      labelText: 'Larger storage',
-      description: 'Extra 1TB of cloud save',
-      price: { billingPeriod: 'Monthly', amount: 2 },
-    },
-
-    {
-      value: 'customizable-profile',
-      checked: false,
-      onChange: () => {},
-      labelText: 'Customizable Profile',
-      description: 'Custom theme on your profile',
-      price: { billingPeriod: 'Monthly', amount: 2 },
-    },
-  ];
+    return { addOns: addOnsProp, subscription, onSubscriptionChange };
+  })();
 
   return (
     <div className="grid min-h-dvh place-content-center gap-4 p-2">
@@ -267,14 +336,7 @@ export default function App() {
 
             {formProgressActor.snapshot.hasTag('summary') && (
               <Summary
-                subscription={{ name: 'Arcade', billingPeriod: 'Yearly', price: 90 }}
-                addOns={[
-                  { name: 'Online service', price: 10 },
-                  { name: 'Larger storage', price: 20 },
-                ]}
-                onSubscriptionChange={() =>
-                  formProgressActor.send({ type: 'SUMMARY.CHANGE_SUBSCRIPTION' })
-                }
+                {...summaryProps}
                 footer={
                   <div className="flex items-center justify-between">
                     <button
