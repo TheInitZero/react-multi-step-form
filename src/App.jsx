a7c3bff3fb43bec32f2a108eabee3c351b50e60a
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-05-07 16:26:08 +0530

    feat: add markup for SignupFormStepYourInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 export default function App() {
+  let model = {
+    currentStep: 'your-info',
+    statuses: {
+      'your-info': 'started',
+      'select-plan': 'not-started',
+      'add-ons': 'not-started',
+      summary: 'not-started',
+    },
+  };
+
   return (
     <div>
       <header>
         <h1>Signup</h1>
       </header>
 
-      <SignupProgress
-        model={{
-          currentStep: 'your-info',
-          statuses: {
-            'your-info': 'started',
-            'select-plan': 'not-started',
-            'add-ons': 'not-started',
-            summary: 'not-started',
-          },
-        }}
-      />
+      <SignupProgress model={model} />
+
+      <main>
+        <form>
+          <SignupFormStepYourInfo />
+        </form>
+      </main>
     </div>
   );
 }
@@ -62,5 +68,76 @@ function SignupProgress({ model }) {
     <aside aria-label="Signup progress">
       <ol>{steps}</ol>
     </aside>
+  );
+}
+
+function SignupFormStepYourInfo() {
+  return (
+    <fieldset>
+      <div>
+        <legend>Personal info</legend>
+
+        <p>Please provide your name, email address, and phone number.</p>
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="name">Name</label>
+
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="e.g. Stephen King"
+            autocomplete="name"
+            aria-invalid="false"
+            aria-describedby="name-error"
+            required
+          />
+
+          <span id="name-error" aria-live="assertive"></span>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email address</label>
+
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="e.g. stephenKing42@hotmail.com"
+            autocomplete="email"
+            aria-invalid="false"
+            aria-describedby="email-error"
+            required
+          />
+
+          <span id="email-error" aria-live="assertive"></span>
+        </div>
+
+        <div>
+          <label htmlFor="telephone">Phone number</label>
+
+          <input
+            id="telephone"
+            type="tel"
+            name="telephone"
+            placeholder="e.g. +1 234 567 890"
+            autocomplete="tel"
+            aria-invalid="false"
+            aria-describedby="telephone-error"
+            required
+          />
+
+          <span id="telephone-error" aria-live="assertive"></span>
+        </div>
+      </div>
+
+      <div>
+        <button type="button" disabled>
+          Next step
+        </button>
+      </div>
+    </fieldset>
   );
 }
