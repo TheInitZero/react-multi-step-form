b5cf33e116446f234644dd58a6743d33a8bdfdb9
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2026-05-09 22:37:51 +0530

    refactor: move DATA to data.js

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const DATA = {
+  SUBSCRIPTIONS: {
+    arcade: {
+      id: 'arcade',
+      name: 'Arcade',
+      price: {
+        monthly: 9,
+        yearly: 90,
+      },
+      bonus: '+ 2 months',
+    },
+
+    advanced: {
+      id: 'advanced',
+      name: 'Advanced',
+      price: {
+        monthly: 12,
+        yearly: 120,
+      },
+      bonus: '+ 3 months',
+    },
+
+    pro: {
+      id: 'pro',
+      name: 'Pro',
+      price: {
+        monthly: 15,
+        yearly: 150,
+      },
+      bonus: '+ 4 months',
+    },
+  },
+
+  ADD_ONS: {
+    'online-service': {
+      id: 'online-service',
+      name: 'Online service',
+      price: {
+        monthly: 1,
+        yearly: 10,
+      },
+      detail: 'Access to multiplayer games',
+    },
+
+    'larger-storage': {
+      id: 'larger-storage',
+      name: 'Larger storage',
+      price: {
+        monthly: 2,
+        yearly: 20,
+      },
+      detail: 'Extra 1TB of cloud save',
+    },
+
+    'customizable-profile': {
+      id: 'customizable-profile',
+      name: 'Customizable profile',
+      price: {
+        monthly: 2,
+        yearly: 20,
+      },
+      detail: 'Custom theme on your profile',
+    },
+  },
+};
